b7f33a25504ca9bcc57321b371cf2e4ebe751f9f
Gustavo Machado <gmachad@wolftech.ad.ncsu.edu>
2020-11-20 14:55:02 -0500

    read data

---
+++ codes/data_read.R
@@ -0,0 +1,27 @@
+#packges 
+library(tidyverse)
+library(lubridate)
+library(reshape2)
+library(scales)
+library(igraph)
+library(ggalluvial)
+library(epinemo)
+library(EpiContactTrace)
+library(sf)
+library(readr)
+library(tidyverse);library(tibble)
+library(viridis); library(ggplot2)
+library(lubridate);library(reshape2)
+library(rgdal);library(broom)
+library(readr); library(igraph)
+library(reshape2);library(sf)
+library(ggpubr); library(tidygraph);
+library(ggraph);library(ggpubr)
+library(epicontacts)
+library(openxlsx);library(lubridate)
+library(gt)
+
+
+# Erase workplace
+rm(list = ls())
+cat("\014")
