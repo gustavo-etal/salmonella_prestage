b7c141d0d151a95b73719ab8ff9bf62adefb66ef
Gustavo Machado <gmachad@wolftech.ad.ncsu.edu>
2020-12-05 09:47:40 -0500

    initial discription

--- codes/data_read.R
+++ codes/data_read.R
@@ -25,3 +25,37 @@ library(gt)
 # Erase workplace
 rm(list = ls())
 cat("\014")
+
+pop<-read_csv("./data/PrestageFarms_list_2020.csv");pop
+
+
+salmo<-read_csv("./data/Growout_Bootie.csv");salmo
+
+# descriptive stats ----
+## now free description
+salmo%>%
+  group_by(Serotype)%>%
+  dplyr::count(Serotype, sort = TRUE)%>%
+  drop_na()%>%
+  dplyr::select(n,Serotype)%>%
+  arrange(desc(n))%>%
+  print(n = 10)
+
+
+salmo%>%
+  group_by(Prod_Type)%>%
+  dplyr::count(Serotype, sort = TRUE)%>%
+  ungroup() %>%
+  drop_na()%>%
+  arrange(desc(n))%>% 
+  ggplot(aes(fct_infreq(Prod_Type,n), n)) +
+  geom_bar(stat="identity") + coord_flip()+
+  scale_y_continuous(breaks = seq(0,2500,by = 10))+
+  labs(x="TProd_Type", y="volume")+
+  theme(text = element_text(size = 12, face = "bold"),
+        legend.title = element_text(size = 12))
+
+ggsave("./Fig/freq_prod_type.tiff",plot = last_plot(), dpi = 300, width = 190, height = 130, units = "mm")
+
+
+
