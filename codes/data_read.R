8fd66fd2cdabf9d9b35d6dbab592886f0a1dc440
Gustavo Machado <gmachad@wolftech.ad.ncsu.edu>
2020-12-10 15:30:35 -0500

    updated the table in the growout and the revalues

--- codes/data_read.R
+++ codes/data_read.R
@@ -20,6 +20,9 @@ library(ggraph);library(ggpubr)
 library(epicontacts)
 library(openxlsx);library(lubridate)
 library(gt)
+require(plyr)
+require(dplyr)
+
 
 
 # Erase workplace
@@ -30,6 +33,68 @@ pop<-read_csv("./data/PrestageFarms_list_2020.csv");pop
 
 
 salmo<-read_csv("./data/Growout_Bootie.csv");salmo
+
+## transform to date formats
+salmo$Start_Date<-as.Date (as.character(salmo$Start_Date), format= "%m/%d/%y")
+salmo$Process_Date<-as.Date (as.character(salmo$Process_Date), format= "%m/%d/%y")
+
+## transform as.factors
+
+salmo<-salmo%>%
+  mutate(Project=`Project#`)%>%
+  select(-`Project#`)
+
+
+salmo$Prod_Type<-as.factor(salmo$Prod_Type)
+salmo$Sex<-as.factor(salmo$Sex)
+salmo$Breed<-as.factor(salmo$Breed)
+salmo$Brooder<-as.factor(salmo$Brooder)
+salmo$Grower<-as.factor(salmo$Grower)
+salmo$Bootie_Swabs<-as.factor(salmo$Bootie_Swabs)
+salmo$Group<-as.factor(salmo$Group)
+salmo$Serotype<-as.factor(salmo$Serotype)
+salmo$Project<-as.factor(salmo$Project)
+str(salmo)
+
+## relevel prod. type
+## check the sex<>production type -- Ask 
+
+salmo<-salmo%>%
+  mutate(Prod_Type = revalue(Prod_Type,
+                                c("Conv"="CONV", "Conv."="CONV")))
+
+## relevel swabs
+salmo%>%
+  mutate(Bootie_Swabs = revalue(Bootie_Swabs,
+                c("positive"="Positive", "Positive"="Positive")))
+
+
+
+## relevel prod. type
+salmo%>%
+  mutate(Bootie_Swabs = revalue(Bootie_Swabs,
+                                c("positive"="Positive", "Positive"="Positive")))
+
+salmo$Bootie_Swabs<-ifelse(salmo$Bootie_Swabs ="NA", Negative, salmo$Bootie_Swabs)
+
+## relevel of Serotype
+salmo%>%
+  mutate(Serotype = revalue(Serotype,
+                                c("16:d:-"="16:d:1,2", "Positive"="Positive")))
+
+
+#16:d:-        --- 16:d:1,2
+#6,7:r:         ---- 6,7:r
+#1,4,5,12:i:-  ---1,4,5,12:i
+#1,4,5,12:      ---1,4,5,12
+#Rough "O":r:1,5    ---Rough O:r:1,5
+#TYPH VAR. O:5-     -----TYPH VAR. O:5
+# Typhimirium       ---Typhimurium
+#OUAKAM             --Ouakam
+#Lillie              -- Lille
+##Bertaq            --Berta          
+
+
 
 # descriptive stats ----
 ## now free description
